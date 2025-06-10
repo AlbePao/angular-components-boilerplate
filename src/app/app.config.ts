7f1fd1488bb7735c06e61e039632c83709fa56b0
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-10 18:07:38 +0200

    main and app component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { jwtInterceptor } from '@lib/interceptors/jwt.interceptor';
+import { serverErrorInterceptor } from '@lib/interceptors/server-error.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptors([serverErrorInterceptor, jwtInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+  ],
 };
