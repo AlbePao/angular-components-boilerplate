77bb42e22bd4fb67a97d9d4cb4793b567fbfdda2
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-12 17:24:10 +0200

    paginator component

---
+++ src/app/lib/components/paginator/paginator.component.ts
@@ -0,0 +1,97 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  numberAttribute,
+  Output,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormFieldModule } from '@lib/components/form-field';
+import { IconComponent } from '@lib/components/icon';
+import { SelectDirective } from '@lib/components/select';
+
+export interface PaginationEvent {
+  currentPage: number;
+  pageSize: number;
+  length: number;
+}
+
+interface PaginationRange {
+  rangeStart: number;
+  rangeEnd: number;
+  length: number;
+}
+
+const PAGINATION_RANGE = 2;
+const ELLIPSIS_RANGE = 3;
+
+@Component({
+  selector: 'app-paginator',
+  standalone: true,
+  imports: [CommonModule, IconComponent, FormsModule, FormFieldModule, SelectDirective],
+  templateUrl: './paginator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginatorComponent {
+  @Input({ transform: numberAttribute }) currentPage = 1;
+  @Input({ transform: numberAttribute }) pageSize = 0;
+  @Input({ transform: numberAttribute }) length = 0;
+  @Input() pageSizeOptions = [25, 50, 100, 150];
+
+  @Output() readonly pageChange = new EventEmitter<PaginationEvent>();
+
+  get pageCounter(): number[] {
+    const { length, pageSize } = this;
+    const totalPages = Math.ceil(length / pageSize);
+
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  get paginationRange(): PaginationRange {
+    const { currentPage, pageSize, length } = this;
+    const rangeEnd = currentPage * pageSize;
+
+    return {
+      rangeStart: length > 0 ? (currentPage - 1) * pageSize + 1 : 0,
+      rangeEnd: rangeEnd > length ? length : rangeEnd || 0,
+      length: length || 0,
+    };
+  }
+
+  @HostBinding('class') classes = 'block';
+
+  protected setPage(newPage: number): void {
+    const { currentPage, pageSize, length } = this;
+
+    if (newPage !== currentPage) {
+      this.pageChange.emit({ currentPage: newPage, pageSize, length });
+    }
+  }
+
+  protected setPageSize(pageSize: number): void {
+    const { length } = this;
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const currentPage = Math.floor(startIndex / pageSize) || 1;
+
+    this.currentPage = currentPage;
+    this.pageSize = pageSize;
+    this.pageChange.emit({ currentPage, pageSize, length });
+  }
+
+  protected isPageInRange(selectedIndex: number): boolean {
+    const { currentPage } = this;
+    const selectedPage = selectedIndex + 1;
+
+    return selectedPage >= currentPage - PAGINATION_RANGE && selectedPage <= currentPage + PAGINATION_RANGE;
+  }
+
+  protected canShowEllipsis(selectedIndex: number): boolean {
+    const { currentPage } = this;
+    const selectedPage = selectedIndex + 1;
+
+    return selectedPage === currentPage - ELLIPSIS_RANGE || selectedPage === currentPage + ELLIPSIS_RANGE;
+  }
+}
---
+++ src/app/lib/components/paginator/paginator.md
@@ -0,0 +1,63 @@
+# Paginator
+
+The `<app-paginator>` component provides navigation for paged information, typically used with a table view or a grid view.
+
+## Inputs
+
+| Input             | Description                                                  | Type       |
+| ----------------- | ------------------------------------------------------------ | ---------- |
+| `currentPage`     | Number of the current page. It starts from 1                 | `number`   |
+| `pageSize`        | Number of items to display on a page                         | `number`   |
+| `pageSizeOptions` | The set of provided page size options to display to the user | `number[]` |
+| `length`          | The total number of items that are being paginated           | `number`   |
+
+## Outputs
+
+| Output       | Description                                       | Type emitted      |
+| ------------ | ------------------------------------------------- | ----------------- |
+| `pageChange` | Event emitted when the paginator changes the page | `PaginationEvent` |
+
+## Interfaces
+
+### `PaginationEvent`
+
+Fired by `pageChange` event
+
+- `currentPage` number of the page currently set
+- `pageSize` number of the current page size
+- `length` number of the total paginated items
+
+## Example
+
+```typescript
+// example.component.ts
+@Component({
+  // other stuff...
+  imports: [
+    // other imports...
+    PaginatorComponent,
+    // other imports...
+  ],
+  // other stuff...
+})
+export class ExampleComponent {
+  currentPage = 1;
+  pageSize = 20;
+  length = 100;
+
+  logPaginator(event: PaginationEvent): void {
+    this.currentPage = event.currentPage;
+    console.log('logPaginator', event);
+  }
+}
+```
+
+```html
+<!-- example.component.html -->
+<app-paginator
+  [currentPage]="currentPage"
+  [pageSize]="pageSize"
+  [length]="length"
+  (pageChange)="logPaginator($event)"
+/>
+```
