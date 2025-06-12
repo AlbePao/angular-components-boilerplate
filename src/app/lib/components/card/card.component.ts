f5c24080c4d4979ebfc96932585461f8f35692ae
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-12 16:01:17 +0200

    card component

---
+++ src/app/lib/components/card/card.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { Colors } from '@lib/types/colors';
+
+// TODO: add app-card-header, app-card-content and app-card-actions directives and related styles
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  template: '<ng-content/>',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+  @Input() color: Colors = 'gray';
+
+  @HostBinding('class')
+  get classes(): string {
+    return `border ${this.borderClass} rounded block`;
+  }
+
+  get borderClass(): string {
+    if (this.color === 'primary') {
+      return 'border-primary';
+    } else if (this.color === 'secondary') {
+      return 'border-secondary';
+    } else if (this.color === 'success') {
+      return 'border-success';
+    } else if (this.color === 'danger') {
+      return 'border-danger';
+    } else if (this.color === 'info') {
+      return 'border-info';
+    }
+
+    return 'border-gray';
+  }
+}
