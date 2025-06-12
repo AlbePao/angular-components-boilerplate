a3ec708dfb343809f17ad679468a775645a996e3
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-12 12:02:52 +0200

    add eslint-plugin-boundaries

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,21 @@
 import eslint from '@eslint/js';
 import angular from 'angular-eslint';
+import boundaries from 'eslint-plugin-boundaries';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
+// eslint-plugin-boundaries based on https://docs.google.com/presentation/d/1oDee5t0cy2t7wEFL9cd2d3_m5Mpj4KfXojp2lGNOiEw/edit?usp=sharing
 export default tseslint.config(
   {
     files: ['**/*.ts'],
+    plugins: { boundaries },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
       ...angular.configs.tsRecommended,
       eslintPluginPrettierRecommended,
+      boundaries.configs.recommended, // all files have to belong to a type
     ],
     processor: angular.processInlineTemplates,
     languageOptions: {
@@ -19,6 +23,27 @@ export default tseslint.config(
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+    settings: {
+      'import/resolver': {
+        // recognise both static and dynamic TS imports
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+      'boundaries/dependency-nodes': ['import', 'dynamic-import'],
+      // types setup
+      'boundaries/elements': [
+        {
+          type: 'lib', // type name
+          pattern: 'lib', // type matching pattern (file path)
+        },
+        {
+          type: 'routes',
+          pattern: 'routes/*', // recognize individual routes
+          capture: ['routes'], // extract routes name into variable
+        }, // eg src/app/routes/product will capture "product"
+      ],
     },
     rules: {
       '@angular-eslint/sort-lifecycle-methods': 'error',
@@ -83,6 +108,25 @@ export default tseslint.config(
       'sort-imports': 'off',
       eqeqeq: 'error',
       curly: 'error',
+      'boundaries/element-types': [
+        'error',
+        {
+          default: 'disallow',
+          rules: [
+            {
+              from: 'lib', // type lib
+              allow: ['lib'], // can only import from itself (lib)
+            },
+            {
+              from: 'routes', // type routes
+              allow: [
+                'lib', // can import from lib
+                ['feature', { feature: '${from.feature}' }],
+              ],
+            },
+          ],
+        },
+      ],
     },
   },
   {
