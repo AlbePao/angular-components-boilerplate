a7872265232d40a338c92d39966f82e2e7127c08
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-16 15:52:42 +0200

    InputDateIsoDirective fix

--- src/app/lib/directives/input-date-iso.directive.ts
+++ src/app/lib/directives/input-date-iso.directive.ts
@@ -7,7 +7,7 @@ import { provideNgValueAccessor } from '@lib/providers/ng-value-accessor';
   selector: 'input[type=date][formControlName], input[type=date][formControl], input[type=date][ngModel]',
   providers: [provideNgValueAccessor(InputDateIsoDirective)],
   host: {
-    '(input)': 'onInput($event.target.valueAsDate)',
+    '(input)': 'onInput($event)',
     '(blur)': 'onTouched()',
   },
 })
@@ -15,8 +15,9 @@ export class InputDateIsoDirective implements ControlValueAccessor {
   private readonly _renderer = inject(Renderer2);
   private readonly _elementRef = inject<ElementRef<HTMLInputElement>>(ElementRef);
 
-  onInput = (date?: Date): void => {
-    const isoString = date ? date.toISOString() : null;
+  onInput = (event: Event): void => {
+    const { valueAsDate } = event.target as HTMLInputElement;
+    const isoString = valueAsDate ? valueAsDate.toISOString() : null;
     this.onChange(isoString);
   };
   onChange = (value: string | null): void => {};
