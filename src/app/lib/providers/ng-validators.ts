b7107b577ac319fc5ef630e431c3a425937c6863
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-17 17:22:04 +0200

    updated types

--- src/app/lib/providers/ng-validators.ts
+++ src/app/lib/providers/ng-validators.ts
@@ -1,7 +1,7 @@
-import { Provider } from '@angular/core';
+import { Provider, Type } from '@angular/core';
 import { NG_VALIDATORS } from '@angular/forms';
 
-export const provideNgValidators = (useExisting: object): Provider => ({
+export const provideNgValidators = (useExisting: Type<unknown>): Provider => ({
   provide: NG_VALIDATORS,
   useExisting,
   multi: true,
--- src/app/lib/providers/ng-value-accessor.ts
+++ src/app/lib/providers/ng-value-accessor.ts
@@ -1,7 +1,7 @@
-import { Provider, forwardRef } from '@angular/core';
+import { Provider, Type, forwardRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
-export const provideNgValueAccessor = (useExisting: object): Provider => ({
+export const provideNgValueAccessor = (useExisting: Type<unknown>): Provider => ({
   provide: NG_VALUE_ACCESSOR,
   useExisting: forwardRef(() => useExisting),
   multi: true,
