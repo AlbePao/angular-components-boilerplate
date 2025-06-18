5d2b0d73b84dd35f89d092ab5b9b4beb745781aa
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-18 18:27:49 +0200

    remove showcase home

--- src/app/routes/showcase/index.ts
+++ src/app/routes/showcase/index.ts
@@ -3,38 +3,38 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    title: 'Showcase',
-    loadComponent: async () => (await import('./showcase.component')).ShowcaseComponent,
+    pathMatch: 'full',
+    redirectTo: '/',
   },
   {
     path: 'accordion-example',
-    title: 'Accordion',
+    title: 'Accordion | Showcase',
     loadComponent: async () =>
       (await import('./accordion-example/accordion-example.component')).AccordionExampleComponent,
   },
   {
     path: 'button-example',
-    title: 'Buttons',
+    title: 'Buttons | Showcase',
     loadComponent: async () => (await import('./button-example/button-example.component')).ButtonExampleComponent,
   },
   {
     path: 'checkbox-example',
-    title: 'Checkbox',
+    title: 'Checkbox | Showcase',
     loadComponent: async () => (await import('./checkbox-example/checkbox-example.component')).CheckboxExampleComponent,
   },
   {
     path: 'radio-example',
-    title: 'Radio',
+    title: 'Radio | Showcase',
     loadComponent: async () => (await import('./radio-example/radio-example.component')).RadioExampleComponent,
   },
   {
     path: 'tabs-example',
-    title: 'Tabs',
+    title: 'Tabs | Showcase',
     loadComponent: async () => (await import('./tabs-example/tabs-example.component')).TabsExampleComponent,
   },
   {
     path: 'toggle-example',
-    title: 'Toggle',
+    title: 'Toggle | Showcase',
     loadComponent: async () => (await import('./toggle-example/toggle-example.component')).ToggleExampleComponent,
   },
 ];
--- src/app/routes/showcase/showcase.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-showcase',
-  imports: [],
-  templateUrl: './showcase.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ShowcaseComponent {}
