caad795a340c391c8fd6ccb64ec8229d06178940
Alberto Paolucci <apaolucci@softstrategy.it>
2025-06-20 17:43:39 +0200

    fix input errors

--- src/app/lib/components/form-field/pipes/control-errors.ts
+++ src/app/lib/components/form-field/pipes/control-errors.ts
@@ -1,1 +1,11 @@
-export type ControlErrors = 'required' | 'pattern' | 'email' | 'minlength' | 'maxlength' | 'min' | 'max';
+import { AUTOCOMPLETE_INPUT_INVALID } from '@lib/components/autocomplete';
+
+export type ControlErrors =
+  | 'required'
+  | 'pattern'
+  | 'email'
+  | 'minlength'
+  | 'maxlength'
+  | 'min'
+  | 'max'
+  | typeof AUTOCOMPLETE_INPUT_INVALID;
