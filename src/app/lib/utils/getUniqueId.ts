b5053f5db28b8522a11241a0f523eb5c9ffb4c81
Alberto Paolucci <apaolucci@softstrategy.it>
2025-09-18 16:06:21 +0200

    refactor: migrated to cdk id generator

--- src/app/lib/utils/getUniqueId.ts
+++ src/app/lib/utils/getUniqueId.ts
@@ -1,6 +1,6 @@
+import { _IdGenerator } from '@angular/cdk/a11y';
 import { inject } from '@angular/core';
-import { IdGeneratorService } from '@lib/services/id-generator.service';
 
 export function getUniqueId(id: string): string {
-  return inject(IdGeneratorService).getId(id);
+  return inject(_IdGenerator).getId(id);
 }
