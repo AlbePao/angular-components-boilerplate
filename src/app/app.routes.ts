dd874898f9615924062682af24fe0a549619b40c
Alberto Paolucci <apaolucci@softstrategy.it>
2026-07-30 18:09:09 +0200

    routes component lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,36 @@
 import { Routes } from '@angular/router';
 import { authGuard } from '@lib/guards/auth.guard';
-import { HorizontalLayoutComponent } from '@lib/layouts/horizontal-layout';
-import { MainLayoutComponent } from '@lib/layouts/main-layout';
-import { WelcomeLayoutComponent } from '@lib/layouts/welcome-layout';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    loadComponent: () => WelcomeLayoutComponent,
+    loadComponent: async () => (await import('@lib/layouts/welcome-layout')).WelcomeLayoutComponent,
     loadChildren: async () => (await import('@routes/auth')).routes,
     canMatch: [authGuard({ requiresAuthentication: false })],
   },
   {
     path: '',
-    loadComponent: () => HorizontalLayoutComponent,
-    loadChildren: async () => (await import('@routes/home')).routes,
+    loadComponent: async () => (await import('@lib/layouts/horizontal-layout')).HorizontalLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: async () => (await import('@routes/home')).routes,
+      },
+      {
+        path: 'users/:username',
+        loadChildren: async () => (await import('@routes/user')).routes,
+      },
+      {
+        path: 'settings',
+        loadChildren: async () => (await import('@routes/settings')).routes,
+      },
+    ],
     canMatch: [authGuard()],
   },
   {
     path: 'showcase',
-    loadComponent: () => MainLayoutComponent,
+    loadComponent: async () => (await import('@lib/layouts/main-layout')).MainLayoutComponent,
     loadChildren: async () => (await import('@routes/showcase')).routes,
-    canMatch: [authGuard()],
-  },
-  {
-    path: 'users/:username',
-    loadComponent: () => HorizontalLayoutComponent,
-    loadChildren: async () => (await import('@routes/user')).routes,
-    canMatch: [authGuard()],
-  },
-  {
-    path: 'settings',
-    loadComponent: () => HorizontalLayoutComponent,
-    loadChildren: async () => (await import('@routes/settings')).routes,
     canMatch: [authGuard()],
   },
   {
